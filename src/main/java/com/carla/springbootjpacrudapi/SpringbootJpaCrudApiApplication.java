3dd980511f634605fe408a6af3856fc86ba20879
Carla Lira <109699098+carlasunset@users.noreply.github.com>
2026-01-13 13:53:34 -0300

    Initial Spring Boot project setup

---
+++ src/main/java/com/carla/springbootjpacrudapi/SpringbootJpaCrudApiApplication.java
@@ -0,0 +1,13 @@
+package com.carla.springbootjpacrudapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootJpaCrudApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootJpaCrudApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/carla/springbootjpacrudapi/SpringbootJpaCrudApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.carla.springbootjpacrudapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootJpaCrudApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
