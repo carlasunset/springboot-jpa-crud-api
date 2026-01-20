acb15556223c0ec4356e3aac7f53e6db2b8c1f72
Carla Lira <109699098+carlasunset@users.noreply.github.com>
2026-01-20 15:28:23 -0300

    Add Exception handling - findById

---
+++ src/main/java/com/carla/springbootjpacrudapi/services/exceptions/ResourceNotFoundException.java
@@ -0,0 +1,9 @@
+package com.carla.springbootjpacrudapi.services.exceptions;
+
+public class ResourceNotFoundException extends RuntimeException {
+    private static final long serialVersionUID = 1L;
+
+    public ResourceNotFoundException(Object id){
+        super("Resource not found. Id " + id);
+    }
+}
