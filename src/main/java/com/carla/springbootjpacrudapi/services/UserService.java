6e1f0366cee94df3d6735d1d17f8844371da6d76
Carla Lira <109699098+carlasunset@users.noreply.github.com>
2026-01-26 14:06:04 -0300

    Exception handling - update method

--- src/main/java/com/carla/springbootjpacrudapi/services/UserService.java
+++ src/main/java/com/carla/springbootjpacrudapi/services/UserService.java
@@ -4,6 +4,7 @@ import com.carla.springbootjpacrudapi.entities.User;
 import com.carla.springbootjpacrudapi.repositories.UserRepository;
 import com.carla.springbootjpacrudapi.services.exceptions.DatabaseException;
 import com.carla.springbootjpacrudapi.services.exceptions.ResourceNotFoundException;
+import jakarta.persistence.EntityNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
@@ -40,9 +41,13 @@ public class UserService {
     }
 
     public User update(Long id, User user){
-        User entity = repository.getReferenceById(id);
-        updateData(entity, user);
-        return repository.save(entity);
+        try {
+            User entity = repository.getReferenceById(id);
+            updateData(entity, user);
+            return repository.save(entity);
+        } catch (EntityNotFoundException e){
+            throw new ResourceNotFoundException(id);
+        }
     }
 
     private void updateData(User entity, User user){
